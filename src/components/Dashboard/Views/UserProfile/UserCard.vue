e936ef39b807b4dc72599d4bec3edda280e1d8d3
Tim <tjgambs@gmail.com>
2018-04-21 17:54:58 -0500

    UserCard now updates from the store

--- src/components/Dashboard/Views/UserProfile/UserCard.vue
+++ src/components/Dashboard/Views/UserProfile/UserCard.vue
@@ -1,64 +1,36 @@
 <template>
   <card class="card-user">
-    <img slot="image" src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
+    <img slot="image" src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"/>
     <div class="author">
-      <a href="#">
-        <img class="avatar border-gray" src="static/img/faces/face-3.jpg" alt="..."/>
-
-        <h4 class="title">Mike Andrew<br />
-          <small>michael24</small>
+      <a>
+        <img class="avatar border-gray" :src="avatarUrl"/>
+        <h4 class="title">{{ completeName }}<br />
+          <small>{{ email }}</small>
         </h4>
       </a>
-    </div>
-    <p class="description text-center"> "Lamborghini Mercy <br>
-      Your chick she so thirsty <br>
-      I'm in that two seat Lambo"
-    </p>
-    <div slot="footer" class="text-center">
-      <button href="#" class="btn btn-simple"><i class="fa fa-facebook-square"></i></button>
-      <button href="#" class="btn btn-simple"><i class="fa fa-twitter"></i></button>
-      <button href="#" class="btn btn-simple"><i class="fa fa-google-plus-square"></i></button>
     </div>
   </card>
 </template>
 <script>
   import Card from 'src/components/UIComponents/Cards/Card.vue'
+  import store from '../../../../store'
   export default {
+    store,
     components: {
       Card
     },
-    data () {
-      return {
-        details: [
-          {
-            title: '12',
-            subTitle: 'Files'
-          },
-          {
-            title: '2GB',
-            subTitle: 'Used'
-          },
-          {
-            title: '24,6$',
-            subTitle: 'Spent'
-          }
-        ]
-      }
-    },
-    methods: {
-      getClasses (index) {
-        var remainder = index % 3
-        if (remainder === 0) {
-          return 'col-md-3 col-md-offset-1'
-        } else if (remainder === 2) {
-          return 'col-md-4'
-        } else {
-          return 'col-md-3'
-        }
+    computed: {
+      completeName: function() {
+        return store.state.user.firstName + ' ' + store.state.user.lastName;
+      },
+      email: function() {
+        return store.state.user.email;
+      },
+      avatarUrl: function() {
+        return "https://api.adorable.io/avatars/285/" + this.email + ".png";
       }
     }
   }
-
 </script>
 <style>
 
