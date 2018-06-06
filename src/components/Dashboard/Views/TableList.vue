2605adfc377aa10f04c90cd08d94670d62c08cc2
Tim <tjgambs@gmail.com>
2018-06-05 21:44:05 -0500

    Cleaned up

--- src/components/Dashboard/Views/TableList.vue
+++ src/components/Dashboard/Views/TableList.vue
@@ -1,64 +1,62 @@
 <template>
 	<div class="content">
-    <div class="container-fluid">
-      <div class="row">
- <div class="col-md-12">
-
-  <card>
-  	<template slot="header">
-        <h4 class="card-title">Search Courses</h4>
-    </template>
-	<form>  
-	  
-	  <div class="row">
-		<div class="col-md-2">
-			<label class="control-label">
-			  Course Subject
-			</label>
-			<select v-model="course_subject" class="form-control">
-			  <option value=""></option>
-			  <option v-for="item in options.course_subject" v-bind:value="item">{{item}}</option>
-			</select>
+    	<div class="container-fluid">
+      		<div class="row">
+ 				<div class="col-md-12">
+				    <card>
+					  	<template slot="header">
+					        <h4 class="card-title">Search Courses</h4>
+					    </template>
+						<form>  
+					    <div class="row">
+							<div class="col-md-2">
+								<label class="control-label">Course Subject</label>
+								<select v-model="courseSubject" class="form-control">
+							  		<option value=""></option>
+							  		<option v-for="item in options.courseSubject" v-bind:value="item">{{item}}</option>
+								</select>
+							</div>
+						
+							<div class="col-md-2">
+						  		<fg-input type="text"
+								    label="Course Number"
+									placeholder=""
+									v-model="courseNumber"
+									>
+								</fg-input>
+							</div>
+					  	</div>
+						</form>
+				  
+						<div class="text-center">
+					        {{ infoMessage }}
+					        <button type="submit" class="btn btn-info btn-fill float-right" v-on:click="searchCourses">
+					          Search
+					        </button>
+					    </div>
+					    <div class="clearfix"></div>	  
+					</card>
+				    <card v-if="results.length > 0">
+						<template slot="header">
+				    		<h4 class="card-title">Search Results</h4>
+				    	</template>
+					  
+						<div v-for="item in results">
+					        <div class="col-12">
+					            <h3>{{ item.subject + ' ' + item.catalog_nbr + ' - ' + item.title }}</h3>
+					            <p>{{ item.description + ' ' + ((item && item.prerequisites) ? item.prerequisites : '') }}</p>
+								<button type="submit" class="btn btn-info btn-fill float-right" v-on:click="addToWishList(item.subject, item.catalog_nbr, item.title)">
+								  Add to Wish List
+								</button>
+								<br /><br />
+								<hr />
+				        	</div>
+				      	</div>
+				    </card>
+				</div>
+			</div>
 		</div>
-		
-		<div class="col-md-2">
-		  <fg-input type="text"
-		    label="Course Number"
-			placeholder=""
-			v-model="course_number"
-			>
-		  </fg-input>
-		</div>
-	  </div>
-	</form>
-  
-	  <div class="text-center">
-        {{ errorMessage }}
-        <button type="submit" class="btn btn-info btn-fill float-right" v-on:click="searchCourses">
-          Search
-        </button>
-      </div>
-      <div class="clearfix"></div>	  
-	</card>
-    <card v-if="results.length > 0">
-	  <template slot="header">
-        <h4 class="card-title">Search Results</h4>
-      </template>
-	  
-	  <div v-for="item in results">
-        <div class="col-12">
-            <h3>{{ item.subject + ' ' + item.catalog_nbr + ' - ' + item.title }}</h3>
-            <p>{{ item.description + ' ' + ((item && item.prerequisites) ? item.prerequisites : '') }}</p>
-			<button type="submit" class="btn btn-info btn-fill float-right" v-on:click="addToWishList(item.subject, item.catalog_nbr, item.title)">
-			  Add to Wish List
-			</button>
-			<br /><br />
-			<hr />
-        </div>
-      </div>
-  </card>
-</div></div></div>
-</div>
+	</div>
 </template>
 <script>
   import LTable from 'src/components/UIComponents/Table.vue'
@@ -81,92 +79,42 @@
     data () {
       return {
 		results: [],
-		course_subject: '',
-		course_number: '',
-		myToggle: 'blank',
-        displayTable: {
-          columns: [...tableColumns],
-          rows: [...tableData]
-        },
-		errorMessage: '',
+		courseSubject: '',
+		courseNumber: '',
+		infoMessage: '',
 		options: {
-		  course_subject: ['CNS', 'CSC', 'ECT', 'GAM', 'GEO', 'GPH', 'HCI', 'HIT', 'IPD', 'IS', 'IT', 'PM', 'SE', 'TDC'],
-		  quarters: [
-			  {
-				"description": "2016-2017 Winter", 
-				"stream": "0980"
-			  }, 
-			  {
-				"description": "2016-2017 Spring", 
-				"stream": "0985"
-			  }, 
-			  {
-				"description": "2016-2017 Summer", 
-				"stream": "0990"
-			  }, 
-			  {
-				"description": "2017-2018 Autumn", 
-				"stream": "1000"
-			  }, 
-			  {
-				"description": "2017-2018 Winter", 
-				"stream": "1005"
-			  }, 
-			  {
-				"description": "2017-2018 Spring", 
-				"stream": "1010"
-			  }, 
-			  {
-				"description": "2017-2018 Summer", 
-				"stream": "1015"
-			  }, 
-			  {
-				"description": "2018-2019 Autumn", 
-				"stream": "1020"
-			  }, 
-			  {
-				"description": "2018-2019 Graduate DEC Session", 
-				"stream": "1024"
-			  }
-			]
+		  courseSubject: ['CNS', 'CSC', 'ECT', 'GAM', 'GEO', 'GPH', 'HCI', 'HIT', 'IPD', 'IS', 'IT', 'PM', 'SE', 'TDC'],
 		}
       }
 	},
 	computed: {
 	  setSearchURL: function () {
-	    
-
-		if (this.course_subject != '' && this.course_number != '') {
-			return API_URL + '/v1/search/by_subject_number/' + this.course_subject + '/' + this.course_number;
-		} else if (this.course_subject != '' && this.course_number == '') {
-			return API_URL + '/v1/search/by_subject/' + this.course_subject;
-		} else if (this.course_subject == '' && this.course_number != '') {
-			return API_URL + '/v1/search/by_number/' + this.course_number;
-		} else if (this.course_subject == '' && this.course_number == '') {
-			return API_URL + '/v1/search/all_subjects';
+		if (this.courseSubject != '' && this.courseNumber != '') {
+			return API_URL + '/v1/search/by_subject_number/' + this.courseSubject + '/' + this.courseNumber;
+		} else if (this.courseSubject != '' && this.courseNumber == '') {
+			return API_URL + '/v1/search/by_subject/' + this.courseSubject;
+		} else if (this.courseSubject == '' && this.courseNumber != '') {
+			return API_URL + '/v1/search/by_number/' + this.courseNumber;
 		}
-		
-		return;
 	  }
 	},
 	methods: {
 	  searchCourses () {
-        this.errorMessage = '';
+        this.infoMessage = '';
         this.$http.get(this.setSearchURL)
           .then((response) => {
             this.results = response.data.data.results;
-        }).catch((errors) => {
-            this.errorMessage = 'No data found';
         })
 	  },
 	  addToWishList (subject, number, title) {
-        this.errorMessage = '';
+        this.infoMessage = '';
         this.$http.get(API_URL + '/v1/user/add_to_wishlist/' + subject + ' ' + number + '/' + title)
           .then((response) => {
-            this.errorMessage = 'Added to Wish List';
+            this.infoMessage = 'Added ' + subject + ' ' + number + ' to Wish List';
             var _this = this;
-        }).catch((errors) => {
-            this.errorMessage = 'Already in Wish List';
+            setTimeout(function(){ 
+              _this.infoMessage = '';
+            }, 2000);
         })
       }
 	}
